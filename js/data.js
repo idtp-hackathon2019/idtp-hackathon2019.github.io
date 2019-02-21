b5dadabe34259cbbaa59f34058ad5b285bd03b6e
Jack Manzella <jack.manzella@geappliances.com>
2019-02-21 10:06:45 -0500

    k

--- js/data.js
+++ js/data.js
@@ -42,38 +42,6 @@ function changeDisplay(name){
     console.log(name)
 }
 
-
-function populateTabs(data){
-
-    var secondTab = document.getElementById("secondTab");
-
-    for (let i = 0; i < data.length; i++) {
-        console.log(i);
-        let name = data[i]['itemName'];
-        let upc =  data[i]['upc'];
-        let scannedDate =  data[i]['items'][0]['scannedDateTime'];
-        let nutritionData = data[i]['nutritionData'];
-        console.log(name);
-        let tmp = "tab-item" + i;
-        if (name !== "undefined") {
-            var newElement = document.createElement('DIV');
-            newElement.id = tmp;
-            newElement.className = "tab-item";
-            newElement.innerHTML = name;
-            secondTab.appendChild(newElement);
-            document.getElementById(tmp).addEventListener("click", function(){
-                displayData(nutritionData)
-            },false);
-        }
-    }
-    // var newElement = document.createElement('div');
-    // firstTab.appendChild(newElement);​​​​​​​​​​​​​​​​
-}
-
-function changeDisplay(name){
-    console.log(name)
-}
-
 function getCurrentItems() {
     return $.ajax({
         url: 'https://aromn32tn6.execute-api.us-east-1.amazonaws.com/dev/get-current-items',
